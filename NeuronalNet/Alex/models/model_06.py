801804de7aa946297ad7bd0010cfa540fa17646c
Alex-PC\Alex <alexderta@gmail.com>
2017-06-21 23:42:22 +0200

    präsentation

--- NeuronalNet/Alex/models/model_06.py
+++ NeuronalNet/Alex/models/model_06.py
@@ -2,21 +2,25 @@
 # in this file specific layers can be defined and changed
 # the default data contains 40 x 1200 x 3 data as defined by the input dataformat
 # if the data for test and validation is change the first layer format can change
-# model contains a sequential keras model that can be applied with different layers
+# model contains a sequential keras model that can be applied with different layer
 
-model.add(Conv2D(8, kernel_size=(3, 3),
+# optimizer https://youtu.be/VINCQghQRuM?t=1007
+
+model.add(Conv2D(4, kernel_size=(3, 3),
 				 activation='relu',
 				 input_shape=(40,1200,3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(16, kernel_size=(3, 3),
+model.add(Conv2D(8, kernel_size=(3, 3),
 				 activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(24, kernel_size=(3, 3),
+model.add(Conv2D(16, kernel_size=(3, 3),
 				 activation='sigmoid'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 
 model.add(Flatten())
 model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.4))
 model.add(Dense(len(classes), activation='softmax'))
 
 model.compile(loss=keras.losses.mean_squared_error, optimizer="rmsprop", metrics=['accuracy'])
--- NeuronalNet/Alex/stat.py
+++ NeuronalNet/Alex/stat.py
@@ -1,22 +1,12 @@
 # include data pipe
 from dataloader import plot
 
-plot.plot_csv_multipath("models/model_01/plot.csv", figure="Model_1").savefig("model1.png")
-plot.plot_csv_multipath("models/model_02/plot.csv", figure="Model_2").savefig("model2.png")
-plot.plot_csv_multipath("models/model_03/plot.csv", figure="Model_3").savefig("model3.png")
-plot.plot_csv_multipath("models/model_04/plot.csv", figure="Model_4").savefig("model4.png")
-plot.plot_csv_multipath("models/model_05/plot.csv", figure="Model_5").savefig("model5.png")
-plot.plot_csv_multipath("models/model_07/plot.csv", figure="Model_7").savefig("model7.png")
-plot.plot_csv_multipath("models/model_08/plot.csv", figure="Model_8").savefig("model8.png")
-plot.plot_csv_multipath("models/model_09/plot.csv", figure="Model_9").savefig("model9.png")
-plot.plot_csv_multipath("models/model_10/plot.csv", figure="Model_10").savefig("model10.png")
-plt = plot.plot_csv_multipath("models/model_06/plot.csv", figure="Model_6")
-plt.savefig("model6.png")
-plt.show();
+for i in range(10):
+	plot.plot_csv_multipath("models/model_0"+str(i)+"/plot.csv", figure="Model_"+str(i)).savefig("model"+str(i)+".png")
 
 
-from modelpipe import pipe
-from keras.utils import plot_model
+#from modelpipe import pipe
+#from keras.utils import plot_model
 
 #model = pipe.Pipe().get_model(model_name="models/model_01")
 #plot_model(model, to_file='model.png', show_layer_names=True, show_shapes=True)
