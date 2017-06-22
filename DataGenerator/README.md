1149e56449a715729fd19a32993e3361a76eea0b
Oliver Feucht <olfeit01@hs-esslingen.de>
2017-06-22 23:25:32 +0200

    Changed README.md for DataGenerator

--- DataGenerator/README.md
+++ DataGenerator/README.md
@@ -1,14 +1,24 @@
-# Data Generator
-Das Programm ermöglicht die Erzeugung von Trainings- und Testbildern für den Fontinator.
-Die Bilder bestehen aus einem einzeiligen Text mit unterschiedlichen Schriftstilen (Fonts). 
+# Data Generator (Autor: O.Feucht)
+Das Programm-Modul ermöglicht die Erzeugung von Trainings- und Testbildern für den Fontinator.
+Die Bilder bestehen aus einem einzeiligen Text mit unterschiedlichen Schriftstilen (Fonts).
+Der erzeugte Text wird zufällig erzeugt.
+Für eine nähere Beschreibung siehe Präsentationfolien (S. 5-8)
 
 ## Konfiguration:
 Die Konfiguration erfolgt in der Datei 'config.py'.
-Es kann beispielsweise die Zahl der zu erzeugenden Bilder festgelegt werden.
+Es kann beispielsweise die Zahl der zu erzeugenden Bilder oder die Fontgröße festgelegt werden.
 Die zu verwendenden Fonts werden standardmäßig aus dem Ordner 'fonts' gelesen.
 Die Fonts müssen im .ttf Format vorliegen.
 Die erzeuten Bilderdaten werden standardmäßig im Ordner 'images' abgelegt.
+Im Ordner "text_res" befinden sich einige Textdateien, welche die Wörter für die Zufallssätze enthalten.
 
-## Starten
+## Ausführung
 Zum Starten muss das Skript "createImages.py" ausgeführt werden.
 Es werden alle Fonts eingelesen und die Trainings- und Testbilder erzeugt.
+
+# Libs
+In dem ordner "libs" befinden sich alle vom  Data Generator verwendeten Hilfsklassen.
+
+## WordDict
+Die Klasse "WordDict" ermöglicht die Erzeugung von Zufallssätzen.
+Die Zufallswörter können aus einer Textdatei eingelesen werden.
--- DataGenerator/libs/WordDict.py
+++ DataGenerator/libs/WordDict.py
@@ -1,11 +1,25 @@
 import random as rand
 
 class WordDict:
+    """
+    Allows creating a random sentence.
+    """
+
     def __init__(self, word_dict: list):
+        """
+        Creates an WordDict object from a list of words.
+        which will be used as random word source
+        :param word_dict: A list of words
+        """
         self._ger_word_dict = word_dict
 
-    # Creates an WordDict object
     def load_from_textfile(input_file_path: str, enc="UTF8"):
+        """
+        Creates an WordDict object from an text file.
+        :param input_file_path: The path to the text file
+        :param enc: The encoding of the text file.
+        :return: A WordDict object
+        """
         ger_word_dict = []
         with open(input_file_path, encoding=enc) as f:
             for line in f:
@@ -14,8 +28,12 @@ class WordDict:
                 ger_word_dict.extend(words)
         return WordDict(ger_word_dict)
 
-    # Creates an sentence with <word_count> words
     def get_sentence(self, word_count: int):
+        """
+        Creates an sentence with <word_count> words
+        :param word_count: The number of words in the returned sentence
+        :return: A string containing random words
+        """
         sentence = ""
         for i in range(word_count):
             r_int = rand.randint(0, len(self._ger_word_dict) - 1)
@@ -23,6 +41,9 @@ class WordDict:
             sentence += rand_word + " "
         return sentence
 
-    # Returns the size of word dict
     def get_word_count(self):
+        """
+        Returns the size of word dict
+        :return: The size of words in the WordDict
+        """
         return len(self._ger_word_dict)
